a20aa7f86ce037685942bdac9b40249ae9101b7c
stevenj <stevenj@alum.mit.edu>
2000-06-04 00:09:08 -0400

    added copyright info

--- harminv-main.c
+++ harminv-main.c
@@ -1,9 +1,27 @@
+/* Copyright (C) 2000 Massachusetts Institute of Technology.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
 #include "harminv.h"
 #include "check.h"
+#include "copyright.h"
 
 /* eat whitespace, including #... comments, from the file.  Returns the
    number of newlines read (so that a line count can be maintained).  If
@@ -87,11 +105,6 @@ void usage(FILE *f)
 }
 
 #define TWOPI 6.2831853071795864769252867665590057683943388
-
-#define VERSION "0.1"
-#define COPYRIGHT \
-"Copyright (c) 2000 Massachusetts Institute of Technology.\n" \
-"Please don't distribute this at all yet.\n"
 
 int main(int argc, char **argv)
 {
