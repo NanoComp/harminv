347bbe4b29975fa26be806bdf682894db51cceed
stevenj <stevenj@alum.mit.edu>
2007-03-06 12:11:11 -0500

    update how Changelog is built

--- mkdist.sh
+++ mkdist.sh
@@ -3,16 +3,10 @@
 cvs update -d
 
 # hackery to build ChangeLog
-echo >ChangeLog
-rcs2log -l 72 >ChangeLog
-emacs -batch -q -no-site-file --eval \
-   '(progn (find-file "ChangeLog") (fill-region 1 1000000) (save-buffer))'
+darcs changes --summary > ChangeLog
 
 sh autogen.sh
 
 make maintainer-clean
 ./configure --enable-maintainer-mode
 make dist
-
-rm ChangeLog
-cvs update ChangeLog
